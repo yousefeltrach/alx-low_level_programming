3e9da8af6066b21ae7b0f567414812b38cf95e9c
yousefeltrach <youssef.eltrach@gmail.com>
2023-03-17 09:01:21 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
-}	
+ #include <stdio.h>
+  2 /**
+  3  * main - Entry point
+  4  *
+  5  * Return: Always 0 (Success)
+  6  */
+  7 int main(void)
+  8 {
+  9       puts("\"Programming is like building a multilingual puzzle");
+ 10       return (0);
+ 11 }
