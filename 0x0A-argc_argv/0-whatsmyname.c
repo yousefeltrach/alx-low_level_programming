788e3c857eee73d3e1a2b71593d082f3aa982256
yousefeltrach <youssef.eltrach@gmail.com>
2023-04-08 05:34:08 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@ main - prints the name of the program
 @argv: array of arguments
 Return: Always 0 (Success)
 */
-int main(int argc attribute((unused)), char *argv[])
+int main(int argc attribute((unused)), char *argv[]);
 {
-printf("%s\n", *argv);
+printf("%s\n"; *argv);
 return (0);
 }
