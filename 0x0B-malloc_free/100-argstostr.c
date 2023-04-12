d619577277d809d23c9961e9f6c2631b50ac7d78
yousefeltrach <youssef.eltrach@gmail.com>
2023-04-12 06:53:58 -0700

    5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,48 +2,40 @@
 #include <stdlib.h>
 
 /**
- * argstostr - Concatenates all arguments of the program into a single string.
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
  *
- * @ac: Number of arguments.
- * @av: Array of arguments.
- *
- * Return: A pointer to the concatenated string of arguments.
+ * Return: 0
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, len = 0;
+    int i, n, r = 0, l = 0;
     char *str;
 
     if (ac == 0 || av == NULL)
         return (NULL);
 
-    /* Calculate the total length of all arguments */
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-            len++;
-        len++; /* Add 1 to account for the space after each argument */
+    for (i = 0; i < ac; i++) {
+        for (n = 0; av[i][n]; n++)
+            l++;
     }
-    len++; /* Add 1 for the null terminator */
+    l += ac;
 
-    /* Allocate memory for the concatenated string */
-    str = malloc(sizeof(char) * len);
+    str = malloc(sizeof(char) * l + 1);
+
     if (str == NULL)
         return (NULL);
 
-    /* Concatenate the arguments into the string */
-    len = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-            str[len] = av[i][j];
-            len++;
+    for (i = 0; i < ac; i++) {
+        for (n = 0; av[i][n]; n++) {
+            str[r] = av[i][n];
+            r++;
         }
-        str[len] = ' '; /* Add a space after each argument */
-        len++;
+        if (str[r] == '\0') {
+            str[r++] = '\n';
+        }
     }
-    str[len] = '\0'; /* Add the null terminator */
 
     return (str);
 }
