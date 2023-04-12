9236cf9c4e2d6019c6230f3c27956bd9433fee07
yousefeltrach <youssef.eltrach@gmail.com>
2023-04-12 05:24:16 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,21 +1,49 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * *create_array - creates an array of chars, and initializes
- *  it with a specific char.
- *  @c: char to initialize
- *  @size: number of bytes to allocate
- *
- *  Return: a pointer to the array or null if it fails
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
-char *create_array(unsigned int size, char c)
-{
-	char *array = malloc(size);
 
-	if (size == 0 || array == 0)
-		return (NULL);
-	while (size--)
-		array[size] = c;
-	return (array);
+char *str_concat(char *s1, char *s2)
+{
+    char *conct;
+    int i, ci;
+
+    if (s1 == NULL)
+        s1 = "";
+
+    if (s2 == NULL)
+        s2 = "";
+
+    i = ci = 0;
+    while (s1[i] != '\0')
+        i++;
+
+    while (s2[ci] != '\0')
+        ci++;
+
+    conct = malloc(sizeof(char) * (i + ci + 1));
+
+    if (conct == NULL)
+        return (NULL);
+
+    i = ci = 0;
+    while (s1[i] != '\0')
+    {
+        conct[i] = s1[i];
+        i++;
+    }
+
+    while (s2[ci] != '\0')
+    {
+        conct[i] = s2[ci];
+        i++, ci++;
+    }
+
+    conct[i] = '\0';
+    return (conct);
 }
