958fd2dcf921dc3922ea9c4eb3d6850c8b404a1e
yousefeltrach <youssef.eltrach@gmail.com>
2023-04-14 11:05:40 -0700

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,42 +1,52 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * _realloc - ...
+  * @ptr: ...
+  * @old_size: ...
+  * @new_size: ...
+  *
+  * Return: ...
+  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    char *ptr1;
-    char *old_ptr;
-    unsigned int i;
+	char *nptr;
+	unsigned int i;
 
-    if (new_size == old_size)
-        return (ptr);
+	if (new_size == old_size)
+		return (ptr);
 
-    if (new_size == 0 && ptr)
-    {
-        free(ptr);
-        return (NULL);
-    }
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
 
-    if (!ptr)
-        return (malloc(new_size));
+		if (nptr == NULL)
+			return (NULL);
 
-    ptr1 = malloc(new_size);
-    if (!ptr1)
-        return (NULL);
+		return (nptr);
+	}
+	else
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
 
-    old_ptr = ptr;
+	nptr = malloc(new_size);
 
-    if (new_size < old_size)
-    {
-        for (i = 0; i < new_size; i++)
-            ptr1[i] = old_ptr[i];
-    }
+	if (nptr == NULL)
+		return (NULL);
 
-    if (new_size > old_size)
-    {
-        for (i = 0; i < old_size; i++)
-            ptr1[i] = old_ptr[i];
-    }
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		nptr[i] = ((char *) ptr)[i];
+	}
 
-    free(ptr);
-    return (ptr1);
+	free(ptr);
+	return (nptr);
 }
